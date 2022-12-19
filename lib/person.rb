51f8c6ebbe91916be83cea2f9cf55749b3ddbb1c
David Whitney <codifyamplify@gmail.com>
2022-12-19 18:41:58 -0500

    commit tree 73eed55f3f11c2d737df10aae4a93cc8194e1416

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,19 @@
 class Person
+    # setter method for name equals
+    def name=(name)
+        @name = name
+    end
+    # getter method for name
+    def name
+        @name
+    end
 
+    # setter method for job equals
+    def job=(job)
+        @job = job
+    end
+    # getter method for job
+    def job
+        @job
+    end
 end
